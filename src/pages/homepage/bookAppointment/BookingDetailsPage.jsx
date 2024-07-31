c64f07a9e635a65250dfe9d384f38de303e2c28a
Vinay Kumar <tyagivinay807@gmail.com>
2024-08-01 03:10:10 +0530

    updated BookingDetails.jsx

--- src/pages/homepage/bookAppointment/BookingDetailsPage.jsx
+++ src/pages/homepage/bookAppointment/BookingDetailsPage.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Header from "../../../components/common/Header";
 
+// import doctor from "../../../data/doctors";
+
+//assets
+import Location from "../../../assets/svgs/Location.svg";
+import ApplyCoupon from "../../../assets/svgs/ApplyCoupon.svg";
+import BookingUser from "../../../assets/svgs/BookingUser.svg";
+import BookingTimeIcon from "../../../assets/svgs/BookingTimeIcon.svg";
+import Info from "../../../assets/svgs/Info.svg";
+import Whatsapp from "../../../assets/svgs/Whatsapp.svg";
+
 const BookingDetailsPage = () => {
   const { specialty, doctorName, doctorId } = useParams();
 
@@ -30,12 +40,12 @@ const BookingDetailsPage = () => {
   };
 
   return (
-    <div className="h-screen max-w-sm mx-auto shadow-md rounded-lg">
-        <header className="pt-[60px]">
+    <div className="h-screen max-w-sm mx-auto">
+      <header className="pt-[60px]">
         <Header title="Booking Details" onClick={handleAppointmentClick} />
       </header>
-      <main className="flex-1 pb-28 overflow-y-auto p-4">
-        <div className="flex items-center mb-4">
+      <main className="flex-1 pb-28 overflow-y-auto py-4">
+        <div className="flex items-center mb-4 mx-4">
           <img
             className="rounded-full mr-4 w-12 h-12"
             src={doctor.image}
@@ -45,62 +55,101 @@ const BookingDetailsPage = () => {
             <h3 className="text-lg font-medium">Dr. {doctor.name}</h3>
             <p className="text-gray-500">{doctor.specialty}</p>
           </div>
-          <div className="ml-auto text-blue-500">{doctor.distance}</div>
-        </div>
-        <div className="border-b border-gray-300 mb-4"></div>
-        <div className="mb-4">
-          <p className="text-gray-500">In-clinic appointment booking for</p>
-          <div className="flex items-center">
-            <p className="font-medium">Udit Mahajan, M, 22 yrs</p>
-            <button className="text-blue-500 ml-2">Change</button>
+          <div className="ml-auto">
+            <img src={Location} className="mx-auto" alt="" />
+            <div className="text-blue-500 font-medium">{doctor.distance}</div>
           </div>
         </div>
-        <div className="mb-4">
-          <p className="text-gray-500">Appointment Time</p>
-          <p className="font-medium">
-            Sat, 9 Sep 11:30 AM <span className="text-gray-500">| Today</span>
-          </p>
-        </div>
-        <div className="border-b border-gray-300 mb-4"></div>
-        <div className="mb-4">
-          <div className="flex items-center justify-between border border-gray-300 rounded-lg p-4">
-            <div>
-              <p className="font-medium">Apply Coupon</p>
-              <p className="text-gray-500">
-                Unlock Offers & Discounts with coupon codes
+        <div className="border-b border-gray-300 mb-4 mx-4"></div>
+        <div className=" mx-4">
+          <div className="mb-4">
+            <div className="flex gap-2 text-sm mb-2">
+              <img src={BookingUser} alt="" />
+              <p className="text-[#7a7a7a]">
+                In-clinic appointment booking for
               </p>
             </div>
-            <button className="text-blue-500">APPLY</button>
+            <div className="flex items-center justify-between pr-4">
+              <p className="font-medium flex gap-2 items-center pl-2 text-lg">
+                Udit Mahajan, M, 22 yrs
+              </p>
+              <button className="text-[#00cccc] ml-2 font-medium">
+                Change
+              </button>
+            </div>
+          </div>
+          <div className="border border-gray-300 mb-4"></div>
+
+          <div className="mb-4">
+            <div className="flex gap-2 text-sm mb-2 ">
+              <img src={BookingTimeIcon} alt="" />
+              <p className="text-[#7a7a7a]">Appointment Time</p>
+            </div>
+            <p className="font-medium flex gap-2 items-center pl-2 text-lg">
+              Sat, 9 Sep 11:30 AM{" "}
+              <span className="text-lg text-gray-500 font-normal">|</span>
+              <span className="text-gray-500 flex gap-1 items-center font-normal text-sm">
+                <img src={BookingTimeIcon} className="w-4" alt="" />
+                Today
+              </span>
+            </p>
           </div>
         </div>
-        <div className="mb-4">
+        <div className="border-4 mb-4"></div>
+        <div className="mb-4 mx-4">
+          <div className="flex items-center justify-between border-2 border-[#7a7a7a8e] rounded-lg px-2 py-2">
+            <div className="flex items-start gap-3">
+              <img src={ApplyCoupon} className="mt-1" alt="" />
+              <div className="">
+                <p className="font-medium bg-blue-40 mb-0.5">Apply Coupon</p>
+                <p className="text-[#525252] text-sm bg-yelow-400 font-normal">
+                  Unlock Offers & Discounts with coupon codes
+                </p>
+              </div>
+            </div>
+            <button className="text-[#00cccc] font-medium mr-3">APPLY</button>
+          </div>
+        </div>
+        <div className="mb-4 mx-4">
           <h4 className="font-medium">Bill Details</h4>
-          <div className="flex justify-between mt-2">
-            <p>Consultation Fee</p>
-            <p>₹ 500</p>
+          <div className=" pl-3 text-[#7a7a7a]">
+            <div className="flex justify-between mt-2">
+              <p>Consultation Fee</p>
+              <p>₹ 500</p>
+            </div>
+            <div className="flex justify-between mt-2">
+              <p className="flex items-center gap-2">
+                Service Fee & Tax
+                <img src={Info} alt="" />
+              </p>
+              <div className="flex items-center gap-2"> 
+                <p className="line-through">₹ 49</p>
+                <p className="text-green-500">FREE</p>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-between mt-2">
-            <p>
-              Service Fee & Tax <i className="fas fa-info-circle"></i>
-            </p>
-            <p className="text-green-500">FREE</p>
-          </div>
-          <div className="flex justify-between mt-2 font-medium">
+
+          <div className="border border-gray-300 mb-3 mt-3"></div>
+
+          <div className="flex justify-between mt-2 font-medium pl-3">
             <p>Total Payable</p>
             <p>₹ 500</p>
           </div>
-          <div className="text-green-500 text-center mt-2">
+          <div className="text-[#25d366] text-center mt-4 bg-[#f0fcf4] py-1.5 inline-flex ml-3 px-2 text-sm font-medium border-2 border-dashed border-[#25d366] rounded-lg">
             You have saved ₹ 49 on this appointment
           </div>
         </div>
-        <div className="border-b border-gray-300 mb-4"></div>
-        <div className="mb-4 flex items-center">
+
+        <div className="border-4 mb-4"></div>
+
+        <div className="mb-4 mx-4 flex items-center text-black">
           <input type="checkbox" id="whatsapp" className="mr-2" />
-          <label htmlFor="whatsapp" className="text-gray-500">
+          <label htmlFor="whatsapp" className="text-black flex gap-2 items-start">
             Get notification on Whatsapp
+            <img src={Whatsapp} alt="" />
           </label>
         </div>
-        <div className="mb-4">
+        <div className="mb-4 mx-4">
           <p className="mb-2">Additional Information 1</p>
           <p>Additional Information 2</p>
         </div>
