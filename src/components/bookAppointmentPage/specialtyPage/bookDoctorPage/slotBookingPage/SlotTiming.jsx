5791e6640c9ad2eecb5b34646bcc5a44307dc85d
di-co-der <raghavdivyansh9@gmail.com>
2024-08-02 15:06:19 +0530

    add slot button

--- src/components/bookAppointmentPage/specialtyPage/bookDoctorPage/slotBookingPage/SlotTiming.jsx
+++ src/components/bookAppointmentPage/specialtyPage/bookDoctorPage/slotBookingPage/SlotTiming.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// SlotTiming.jsx
+import React from "react";
 
-function SlotTiming() {
+function SlotTiming({ currentSlots }) {
+  const slotTimes = [
+    "11:00 AM", "11:15 AM", "11:30 AM", "11:45 AM", "12:00 PM",
+    "01:00 PM", "01:15 PM", "01:30 PM", "01:45 PM", "02:00 PM",
+    "02:15 PM", "02:30 PM", "03:00 PM", "06:00 PM", "06:15 PM",
+    "06:30 PM", "06:45 PM", "07:00 PM"
+  ];
+
   return (
-    <div>SlotTiming</div>
-  )
+    <div className="grid grid-cols-3 gap-4 p-4">
+      {slotTimes.slice(0, currentSlots).map((time, index) => (
+        <button
+          key={index}
+          className="bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
+        >
+          {time}
+        </button>
+      ))}
+    </div>
+  );
 }
 
-export default SlotTiming
+export default SlotTiming;
