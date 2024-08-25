7e943e45613f3daea1a410247539dbc68eccdc2a
Vinay Kumar <tyagivinay807@gmail.com>
2024-08-25 22:12:14 +0530

    updated Smart Health Card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,7 +24,7 @@ const HomePage = () => {
           <AbhaCard />
           <ServiceGrid />
           <ImageSlider />
-          <SmartHealthGrid />
+          {/* <SmartHealthGrid /> */}
           <SmartHealthGridSlider />
           <SymptomsTracker />
           <HealthCheckupSlider />
